2f1688c407d63c78366f2c0e86ab710b9b120d53
Markus Döring <mdoering@gbif.org>
2014-05-13 10:38:40 +0200

    Support crawling of Occurrence and TypesAndSpecimen extension

--- crawler-cli/src/main/java/org/gbif/crawler/dwca/fragmenter/DwcaFragmenterService.java
+++ crawler-cli/src/main/java/org/gbif/crawler/dwca/fragmenter/DwcaFragmenterService.java
@@ -15,12 +15,15 @@ import org.gbif.crawler.constants.CrawlerNodePaths;
 import org.gbif.crawler.dwca.LenientArchiveFactory;
 import org.gbif.dwc.record.Record;
 import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.dwc.terms.GbifTerm;
+import org.gbif.dwc.terms.Term;
 import org.gbif.dwc.text.Archive;
 import org.gbif.dwc.text.StarRecord;
 import org.gbif.utils.file.ClosableIterator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 import com.google.common.cache.CacheBuilder;
@@ -137,7 +140,10 @@ public class DwcaFragmenterService extends AbstractIdleService {
         handleOccurrenceCore(uuid, archive, message);
 
       } else if (archive.getExtension(DwcTerm.Occurrence) != null) {
-        handleOccurrenceExtension(uuid, archive, message);
+        handleOccurrenceExtension(uuid, archive, DwcTerm.Occurrence, message);
+
+      } else if (archive.getExtension(GbifTerm.TypesAndSpecimen) != null) {
+        handleOccurrenceExtension(uuid, archive, GbifTerm.TypesAndSpecimen, message);
 
       } else {
         LOG.info("Ignoring DwC-A for dataset [{}] because it does not have Occurrence information.",
@@ -180,35 +186,44 @@ public class DwcaFragmenterService extends AbstractIdleService {
      * Uses a nested iterator over the star and the occurrence extension to produce unique, falttened occurrence
      * fragments.
      */
-    private void handleOccurrenceExtension(UUID uuid, Archive archive, DwcaMetasyncFinishedMessage message) {
-      LOG.info("Fragmenting DwC-A for dataset [{}] with occurrence extension", uuid);
+    private void handleOccurrenceExtension(UUID uuid, Archive archive, Term rowType,
+                                           DwcaMetasyncFinishedMessage message) {
+      LOG.info("Fragmenting DwC-A for dataset [{}] with {} extension", uuid, rowType);
 
       ClosableIterator<StarRecord> iterator = archive.iteratorRaw();
-      int counter = 0;
-      while (iterator.hasNext()) {
-        StarRecord record = iterator.next();
+      try {
+        int counter = 0;
+        while (iterator.hasNext()) {
+          StarRecord record = iterator.next();
 
-        for (Record rec : record.extension(DwcTerm.Occurrence)) {
-          counter++;
-          byte[] serializedRecord = StarRecordSerializer.toJson(record.core(), rec);
-          try {
-            publisher.send(new OccurrenceFragmentedMessage(uuid,
-                                                           message.getAttempt(),
-                                                           serializedRecord,
-                                                           OccurrenceSchemaType.DWCA,
-                                                           EndpointType.DWC_ARCHIVE,
-                                                           message.getValidationReport()));
-          } catch (IOException e) {
-            LOG.error("Could not send message for dataset [{}]", uuid, e);
-          }
-          if (counter % COUNTER_UPDATE_FREQUENCY == 0) {
-            incrementCounters(message, COUNTER_UPDATE_FREQUENCY);
+          List<Record> records = record.extension(rowType);
+          if (records != null) {
+            for (Record rec : record.extension(rowType)) {
+              counter++;
+              byte[] serializedRecord = StarRecordSerializer.toJson(record.core(), rec);
+              try {
+                publisher.send(new OccurrenceFragmentedMessage(uuid,
+                                                               message.getAttempt(),
+                                                               serializedRecord,
+                                                               OccurrenceSchemaType.DWCA,
+                                                               EndpointType.DWC_ARCHIVE,
+                                                               message.getValidationReport()));
+              } catch (IOException e) {
+                LOG.error("Could not send message for dataset [{}]", uuid, e);
+              }
+              if (counter % COUNTER_UPDATE_FREQUENCY == 0) {
+                incrementCounters(message, COUNTER_UPDATE_FREQUENCY);
+              }
+            }
           }
         }
+        incrementCounters(message, counter % COUNTER_UPDATE_FREQUENCY);
+        LOG.info("Successfully extracted [{}] records out of DwC-A for dataset [{}]", counter, uuid);
+
+      } finally {
+        iterator.close();
       }
 
-      incrementCounters(message, counter % COUNTER_UPDATE_FREQUENCY);
-      LOG.info("Successfully extracted [{}] records out of DwC-A for dataset [{}]", counter, uuid);
     }
 
     private void setPagesFragmentedSuccessful(DwcaMetasyncFinishedMessage message) {
--- crawler-cli/src/main/java/org/gbif/crawler/dwca/validator/DwcaValidator.java
+++ crawler-cli/src/main/java/org/gbif/crawler/dwca/validator/DwcaValidator.java
@@ -5,10 +5,12 @@ import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.vocabulary.DatasetType;
 import org.gbif.dwc.record.Record;
 import org.gbif.dwc.terms.DwcTerm;
+import org.gbif.dwc.terms.GbifTerm;
 import org.gbif.dwc.terms.Term;
 import org.gbif.dwc.text.Archive;
 import org.gbif.dwc.text.StarRecord;
 
+import java.util.List;
 import java.util.Set;
 import javax.annotation.Nullable;
 
@@ -19,21 +21,32 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Simple validation of the unique identifiers of a DwC-A.
- *
+ * <p/>
  * This class is only verifying occurrence data so far and passes through checklists without an occurrence extension.
  * Records in the sense of this class are occurrence records, whether they are in the core or as an extension.
  * Therefore the number of validated records for archives with an Occurrence extension is different to the number of
  * core records or rows in the core data file.
  */
 public class DwcaValidator {
+
   private static final Logger LOG = LoggerFactory.getLogger(DwcaValidator.class);
 
   // to conserve memory we won't read more than this
   private static final int MAX_RECORDS = 2000000;
 
+  private int checkedRecords = 0;
+  private int recordsWithInvalidTriplets = 0;
+  private int recordsMissingOccurrenceId = 0;
+  // unique occurrenceIds
+  private Set<String> uniqueOccurrenceIds = Sets.newHashSet();
+  // unique triplets
+  private Set<String> uniqueTriplets = Sets.newHashSet();
+
+  private DwcaValidator() {
+  }
+
   /**
    * Produce a report with the counts of good and bad unique identifiers (triplets and occurrenceId) in the archive.
-   *
    *
    * @param dataset the parent Dataset of the archive
    * @param archive the archive as opened by the dwca-reader project's {@link org.gbif.dwc.text.ArchiveFactory}
@@ -41,44 +54,74 @@ public class DwcaValidator {
    * @return a report with the counts of good, bad and missing identifiers
    */
   public static DwcaValidationReport validate(Dataset dataset, Archive archive) {
-    int checkedRecords = 0;
-    int recordsWithInvalidTriplets = 0;
-    int recordsMissingOccurrenceId = 0;
+    DwcaValidator validator = new DwcaValidator();
+    return validator.check(dataset, archive);
+  }
 
-    // unique occurrenceIds
-    Set<String> uniqueOccurrenceIds = Sets.newHashSet();
-    // unique triplets
-    Set<String> uniqueTriplets = Sets.newHashSet();
-
+  /**
+   * Internal non static working method that does the validation.
+   * If an occurrence core is found this is what gets validated.
+   * Otherwise extension records from either dwc:Occurrence or gbif:TypesAndSpecimen are validated
+   * with Occurrence being the preferred extension.
+   */
+  private DwcaValidationReport check(Dataset dataset, Archive archive) {
     if (dataset.getType() == DatasetType.OCCURRENCE) {
-      for (StarRecord record : archive) {
-        checkedRecords++;
-
-        // triplet
-        String triplet = getTriplet(record.core(), null);
-        if (triplet == null) {
-          recordsWithInvalidTriplets++;
-        } else {
-          uniqueTriplets.add(triplet);
-        }
-
-        // occurrenceId
-        String occurrenceId = record.core().value(DwcTerm.occurrenceID);
-        if (occurrenceId == null) {
-          recordsMissingOccurrenceId++;
-        } else {
-          uniqueOccurrenceIds.add(occurrenceId);
-        }
-
-        if (checkedRecords == MAX_RECORDS) {
-          break;
-        }
-      }
+      validateCore(archive);
 
     } else if (archive.getExtension(DwcTerm.Occurrence) != null) {
-      for (StarRecord star : archive) {
-        // inner loop over occurrence extension records
-        for (Record ext : star.extension(DwcTerm.Occurrence)) {
+      validateExtension(archive, DwcTerm.Occurrence);
+
+    } else if (archive.getExtension(GbifTerm.TypesAndSpecimen) != null) {
+      validateExtension(archive, GbifTerm.TypesAndSpecimen);
+
+    } else {
+      LOG.info("Passing through DwC-A for dataset [{}] because it does not have Occurrence information to validate.",
+               dataset.getKey());
+      return new DwcaValidationReport(dataset.getKey(), 0, 0, 0, 0, 0, false);
+    }
+
+    return new DwcaValidationReport(dataset.getKey(),
+                                    checkedRecords,
+                                    uniqueTriplets.size(),
+                                    recordsWithInvalidTriplets,
+                                    uniqueOccurrenceIds.size(),
+                                    recordsMissingOccurrenceId,
+                                    checkedRecords != MAX_RECORDS);
+  }
+
+  private void validateCore(Archive archive) {
+    for (StarRecord record : archive) {
+      checkedRecords++;
+
+      // triplet
+      String triplet = getTriplet(record.core(), null);
+      if (triplet == null) {
+        recordsWithInvalidTriplets++;
+      } else {
+        uniqueTriplets.add(triplet);
+      }
+
+      // occurrenceId
+      String occurrenceId = record.core().value(DwcTerm.occurrenceID);
+      if (occurrenceId == null) {
+        recordsMissingOccurrenceId++;
+      } else {
+        uniqueOccurrenceIds.add(occurrenceId);
+      }
+
+      if (checkedRecords == MAX_RECORDS) {
+        break;
+      }
+    }
+  }
+
+  private void validateExtension(Archive archive, final Term rowType) {
+    // outer loop over core records, e.g. taxa or samples
+    for (StarRecord star : archive) {
+      // inner loop over extension records
+      List<Record> records = star.extension(rowType);
+      if (records != null) {
+        for (Record ext : records) {
           checkedRecords++;
 
           // triplet can be part of both, e.g. inst and catalog number could be in the core
@@ -102,36 +145,29 @@ public class DwcaValidator {
           }
         }
       }
-
-    } else {
-      LOG.info("Passing through DwC-A for dataset [{}] because it does not have Occurrence information to validate.",
-               dataset.getKey());
-      return new DwcaValidationReport(dataset.getKey(), 0, 0, 0, 0, 0, false);
     }
-
-    return new DwcaValidationReport(dataset.getKey(), checkedRecords, uniqueTriplets.size(), recordsWithInvalidTriplets,
-                                 uniqueOccurrenceIds.size(), recordsMissingOccurrenceId, checkedRecords != MAX_RECORDS);
   }
 
   /**
    * Creates a triplet string if pieces are found in either the core or the occurrence extension.
+   *
    * @return the triplet string or null if it cant be found
    */
-  private static String getTriplet(Record core, Record ext) {
+  private String getTriplet(Record core, Record ext) {
     String institutionCode = valueFromExtOverCore(core, ext, DwcTerm.institutionCode);
     String collectionCode = valueFromExtOverCore(core, ext, DwcTerm.collectionCode);
     String catalogNumber = valueFromExtOverCore(core, ext, DwcTerm.catalogNumber);
 
     if (!Strings.isNullOrEmpty(institutionCode) &&
-         !Strings.isNullOrEmpty(collectionCode) &&
-         !Strings.isNullOrEmpty(catalogNumber)) {
+        !Strings.isNullOrEmpty(collectionCode) &&
+        !Strings.isNullOrEmpty(catalogNumber)) {
       return institutionCode + "§" + collectionCode + "§" + catalogNumber;
     }
     return null;
   }
 
-  private static String valueFromExtOverCore(Record core, @Nullable Record ext, Term term) {
-    if (ext != null && !Strings.isNullOrEmpty(ext.value(term)) ){
+  private String valueFromExtOverCore(Record core, @Nullable Record ext, Term term) {
+    if (ext != null && !Strings.isNullOrEmpty(ext.value(term))) {
       return ext.value(term);
     }
     return core.value(term);
