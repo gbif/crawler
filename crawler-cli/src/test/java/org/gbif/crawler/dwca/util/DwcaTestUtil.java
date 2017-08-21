91bb7abd765704f2c88cdd5f20f5b2def24f53ff
Christian Gendreau <cgendreau@gbif.org>
2017-08-21 19:49:35 +0200

    allow DwcaTestUtil to load a EML file

--- crawler-cli/src/test/java/org/gbif/crawler/dwca/util/DwcaTestUtil.java
+++ crawler-cli/src/test/java/org/gbif/crawler/dwca/util/DwcaTestUtil.java
@@ -9,21 +9,27 @@ import java.util.UUID;
 
 import com.google.common.io.Files;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 public class DwcaTestUtil {
 
   /**
-   * Copies a zip archive from the test resources into a random uuid folder and returns the opened archive.
+   * Copies a zip archive or a single file from the test resources into a random uuid folder and returns the opened archive.
    */
   public static Archive openArchive(String archiveResourcePath) throws IOException {
     UUID uuid = UUID.randomUUID();
 
-    File zipFile = new File(DwcaTestUtil.class.getResource(archiveResourcePath).getFile());
-    File tmpFile = new File(zipFile.getParentFile(), uuid.toString() + ".dwca");
-    Files.copy(zipFile, tmpFile);
+    File srcFile = new File(DwcaTestUtil.class.getResource(archiveResourcePath).getFile());
+    File tmpFile = new File(srcFile.getParentFile(), uuid.toString() + ".dwca");
+    Files.copy(srcFile, tmpFile);
 
     File dwcaDir = new File(tmpFile.getParent(), uuid.toString());
-    return ArchiveFactory.openArchive(tmpFile, dwcaDir);
+    if("zip".equalsIgnoreCase(FilenameUtils.getExtension(srcFile.getName()))) {
+      return ArchiveFactory.openArchive(tmpFile, dwcaDir);
+    }
+
+    return ArchiveFactory.openArchive(tmpFile);
+
   }
 
   public static void cleanupArchive(Archive archive) {
