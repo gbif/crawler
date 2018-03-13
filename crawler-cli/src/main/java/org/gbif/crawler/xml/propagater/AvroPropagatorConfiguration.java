6d01ccaafc146ea334798a0560d1135d57b15051
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-03-13 16:30:49 +0100

    Added toString to the AvroPropagatorConfiguration

--- crawler-cli/src/main/java/org/gbif/crawler/xml/propagater/AvroPropagatorConfiguration.java
+++ crawler-cli/src/main/java/org/gbif/crawler/xml/propagater/AvroPropagatorConfiguration.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParametersDelegate;
+import com.google.common.base.Objects;
 
 public class AvroPropagatorConfiguration {
 
@@ -20,9 +21,17 @@ public class AvroPropagatorConfiguration {
   @NotNull
   public String queueName;
 
-  @Parameter(names = "--thread-count",
-    description = "How many threads should this service use to service requests in parallel")
+  @Parameter(names = "--thread-count", description = "How many threads should this service use to service requests in parallel")
   @Min(1)
   public int threadCount = 1;
+
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this)
+      .add("messaging", messaging)
+      .add("queueName", queueName)
+      .add("threadCount", threadCount)
+      .toString();
+  }
 
 }
