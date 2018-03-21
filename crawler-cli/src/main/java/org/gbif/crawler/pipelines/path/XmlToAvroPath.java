49e94b32642729f6d5936b709de8f2a1624dff6c
Nikolay Volik <nvolik@gbif.org>
2018-03-21 11:54:27 +0100

    #75 Added test cases for the XML callback

--- crawler-cli/src/main/java/org/gbif/crawler/pipelines/path/XmlToAvroPath.java
+++ crawler-cli/src/main/java/org/gbif/crawler/pipelines/path/XmlToAvroPath.java
@@ -10,7 +10,7 @@ class XmlToAvroPath extends ArchiveToAvroPath {
 
   /**
    * input path result example, directory - /mnt/auto/crawler/xml/9bed66b3-4caa-42bb-9c93-71d7ba109dad/2,
-   * if directory is absent, tries get a tar archive  - /mnt/auto/crawler/xml/9bed66b3-4caa-42bb-9c93-71d7ba109dad/2.tar.xz
+   * if directory is absent, tries check a tar archive  - /mnt/auto/crawler/xml/9bed66b3-4caa-42bb-9c93-71d7ba109dad/2.tar.xz
    */
   @Override
   Path buildInputPath(String archiveRepository, UUID dataSetUuid, int attempt){
@@ -19,6 +19,7 @@ class XmlToAvroPath extends ArchiveToAvroPath {
       String filePath = directoryPath.toString() + ".tar.xz";
       Path archivePath = Paths.get(filePath);
       Preconditions.checkState(archivePath.toFile().exists(), "Directory - %s or archive %s does not exist!", directoryPath, archivePath);
+      return archivePath;
     }
     return directoryPath;
   }
