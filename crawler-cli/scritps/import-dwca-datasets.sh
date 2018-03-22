3e53ec2231d7e8916b94116f5aa2b3359516e469
Nikolay Volik <nvolik@gbif.org>
2018-03-22 16:00:22 +0100

    #62 Develop and execute script to import all datasets

---
+++ crawler-cli/scritps/import-dwca-datasets.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Pass as arguments
+# EXAMPLE - source import-dwca-datasets.sh rpathak rpathak http://mq.gbif.org:15672/api/exchanges/%2Fusers%2Frpathak/crawler/publish
+USER=$1
+PASSWORD=$2
+URL=$3
+
+ROUTING_KEY="crawl.dwca.validation.finished"
+CMD="ls dwca"
+
+echo "-----RUN-----"
+
+# All directories in the "dwca" directory
+for UUID in `$CMD`; do
+
+    # The RabbitMQ payload
+    PAYLOAD="{\\\"datasetUuid\\\":\\\"$UUID\\\",\\\"datasetType\\\":\\\"OCCURRENCE\\\",\\\"source\\\":\\\"http://some.new.url\\\",\\\"attempt\\\":1,\\\"validationReport\\\":{\\\"datasetKey\\\":\\\"$UUID\\\",\\\"occurrenceReport\\\":null,\\\"genericReport\\\":null,\\\"invalidationReason\\\":\\\"no reason\\\",\\\"valid\\\":false}}"
+
+    # Create curl command
+    CURL="curl -u $USER:$PASSWORD -H \"content-type:application/json\" -X POST -d'{\"properties\":{\"delivery_mode\":1},\"routing_key\":\"$ROUTING_KEY\",\"payload\":\"$PAYLOAD\",\"payload_encoding\":\"string\"}' $URL"
+
+    # Execute the HTTP request via curl
+    $(eval $CURL)
+
+done
+
+echo "-----FINISHED-----"
---
+++ crawler-cli/scritps/import-xml-datasets.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Pass as arguments
+# EXAMPLE - source import-xml-datasets.sh mlopez mlopez http://mq.gbif.org:15672/api/exchanges/%2Fusers%2Fmlopez/crawler/publish
+USER=$1
+PASSWORD=$2
+URL=$3
+
+ROUTING_KEY="crawl.finished"
+CMD="ls xml"
+
+
+echo "-----RUN-----"
+
+# All directories in the "xml" directory
+for UUID in `$CMD`; do
+
+    # Calculate last attempt
+    ATTEMPT=0
+    for b in `$CMD/$UUID | cut -d '.' -f 1`; do
+      if [ $b -gt $ATTEMPT ]
+      then
+        ATTEMPT=$b
+      fi
+    done
+
+    # The RabbitMQ payload
+    PAYLOAD="{\\\"datasetUuid\\\":\\\"$UUID\\\",\\\"attempt\\\":\\\"$ATTEMPT\\\",\\\"totalRecordCount\\\":\\\"0\\\",\\\"reason\\\":\\\"NORMAL\\\"}"
+
+    # Create curl command
+    CURL="curl -u $USER:$PASSWORD -H \"content-type:application/json\" -X POST -d'{\"properties\":{\"delivery_mode\":1},\"routing_key\":\"$ROUTING_KEY\",\"payload\":\"$PAYLOAD\",\"payload_encoding\":\"string\"}' $URL"
+
+    # Execute the HTTP request via curl
+    $(eval $CURL)
+
+done
+
+echo "-----FINISHED-----"
