ef4921eeb5e9d2c38522e954585b10528f25df63
Nikolay Volik <nvolik@gbif.org>
2018-06-27 13:11:30 +0200

    Moved dwca and xml to avro parts from crawler-cli to pipelines project

--- crawler-cli/src/main/java/org/gbif/crawler/pipelines/service/dwca/DwCAToAvroCallBack.java
+++ crawler-cli/src/main/java/org/gbif/crawler/pipelines/service/dwca/DwCAToAvroCallBack.java
@@ -4,22 +4,15 @@ import org.gbif.common.messaging.AbstractMessageCallback;
 import org.gbif.common.messaging.api.MessagePublisher;
 import org.gbif.common.messaging.api.messages.DwcaValidationFinishedMessage;
 import org.gbif.common.messaging.api.messages.ExtendedRecordAvailableMessage;
+import org.gbif.converter.Dwca2Verbatim;
 import org.gbif.crawler.pipelines.config.ConverterConfiguration;
-import org.gbif.crawler.pipelines.FileSystemUtils;
 import org.gbif.crawler.pipelines.path.ArchiveToAvroPath;
 import org.gbif.crawler.pipelines.path.PathFactory;
-import org.gbif.pipelines.core.io.DwCAReader;
-import org.gbif.pipelines.core.utils.DataFileWriteBuilder;
-import org.gbif.pipelines.io.avro.ExtendedRecord;
-
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Objects;
 import java.util.UUID;
 
-import org.apache.avro.file.DataFileWriter;
-import org.apache.hadoop.fs.FileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,36 +41,14 @@ public class DwCAToAvroCallBack extends AbstractMessageCallback<DwcaValidationFi
 
     ArchiveToAvroPath paths = PathFactory.create(DWCA).from(configuration, datasetUuid, message.getAttempt());
 
-    boolean isFileDeleted;
+    boolean isFileCreated = Dwca2Verbatim.create()
+      .codecFactory(configuration.avroConfig.getCodec())
+      .syncInterval(configuration.avroConfig.syncInterval)
+      .hdfsSiteConfig(configuration.hdfsSiteConfig)
+      .convert(paths.getInputPath(), paths.getOutputPath());
 
-    // the fs has to be out of the try-catch block to avoid closing it, because the hdfs client tries to reuse the
-    // same connection. So, when using multiple consumers, one consumer would close the connection that is being used
-    // by another consumer.
-    FileSystem fs = FileSystemUtils.createParentDirectories(paths.getOutputPath(), configuration.hdfsSiteConfig);
-    try (BufferedOutputStream outputStream = new BufferedOutputStream(fs.create(paths.getOutputPath()));
-         DataFileWriter<ExtendedRecord> dataFileWriter = DataFileWriteBuilder.create()
-           .schema(ExtendedRecord.getClassSchema())
-           .codec(configuration.avroConfig.getCodec())
-           .outputStream(outputStream)
-           .syncInterval(configuration.avroConfig.syncInterval)
-           .build()) {
-
-      DwCAReader reader = new DwCAReader(paths.getInputPath().toString());
-      reader.init();
-      LOG.info("Exporting the DwC Archive to avro {} started", paths.getOutputPath());
-      while (reader.advance()) {
-        dataFileWriter.append(reader.getCurrent());
-      }
-
-    } catch (IOException e) {
-      LOG.error("Failed performing conversion on {}", datasetUuid, e);
-      throw new IllegalStateException("Failed performing conversion on " + datasetUuid, e);
-    } finally {
-      isFileDeleted = FileSystemUtils.deleteAvroFileIfEmpty(fs, paths.getOutputPath());
-    }
-
-    LOG.info("DWCA to avro conversion completed for {}, file was deleted - {}", datasetUuid, isFileDeleted);
-    if (!isFileDeleted && Objects.nonNull(publisher)) {
+    LOG.info("DWCA to avro conversion completed for {}, file was created - {}", datasetUuid, isFileCreated);
+    if (isFileCreated && Objects.nonNull(publisher)) {
       try {
         URI uri = paths.getOutputPath().toUri();
         publisher.send(new ExtendedRecordAvailableMessage(datasetUuid, message.getAttempt(), uri, configuration.interpretTypes));
--- crawler-cli/src/main/java/org/gbif/crawler/pipelines/service/xml/XmlToAvroCallBack.java
+++ crawler-cli/src/main/java/org/gbif/crawler/pipelines/service/xml/XmlToAvroCallBack.java
@@ -4,22 +4,15 @@ import org.gbif.common.messaging.AbstractMessageCallback;
 import org.gbif.common.messaging.api.MessagePublisher;
 import org.gbif.common.messaging.api.messages.CrawlFinishedMessage;
 import org.gbif.common.messaging.api.messages.ExtendedRecordAvailableMessage;
+import org.gbif.converter.Xml2Verbatim;
 import org.gbif.crawler.pipelines.config.ConverterConfiguration;
-import org.gbif.crawler.pipelines.FileSystemUtils;
 import org.gbif.crawler.pipelines.path.ArchiveToAvroPath;
 import org.gbif.crawler.pipelines.path.PathFactory;
-import org.gbif.pipelines.core.utils.DataFileWriteBuilder;
-import org.gbif.pipelines.io.avro.ExtendedRecord;
-import org.gbif.xml.occurrence.parser.ExtendedRecordConverter;
-
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Objects;
 import java.util.UUID;
 
-import org.apache.avro.file.DataFileWriter;
-import org.apache.hadoop.fs.FileSystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,36 +41,16 @@ public class XmlToAvroCallBack extends AbstractMessageCallback<CrawlFinishedMess
 
     ArchiveToAvroPath paths = PathFactory.create(XML).from(configuration, datasetUuid, message.getAttempt());
 
-    boolean isFileDeleted;
+    boolean isFileCreated = Xml2Verbatim.create()
+      .xmlReaderParallelism(configuration.xmlReaderParallelism)
+      .codecFactory(configuration.avroConfig.getCodec())
+      .syncInterval(configuration.avroConfig.syncInterval)
+      .hdfsSiteConfig(configuration.hdfsSiteConfig)
+      .convert(paths.getInputPath(), paths.getOutputPath());
 
-    // the fs has to be out of the try-catch block to avoid closing it, because the hdfs client tries to reuse the
-    // same connection. So, when using multiple consumers, one consumer would close the connection that is being used
-    // by another consumer.
-    FileSystem fs = FileSystemUtils.createParentDirectories(paths.getOutputPath(), configuration.hdfsSiteConfig);
-    try (BufferedOutputStream outputStream = new BufferedOutputStream(fs.create(paths.getOutputPath()));
-         DataFileWriter<ExtendedRecord> dataFileWriter = DataFileWriteBuilder.create()
-           .schema(ExtendedRecord.getClassSchema())
-           .codec(configuration.avroConfig.getCodec())
-           .outputStream(outputStream)
-           .syncInterval(configuration.avroConfig.syncInterval)
-           .build()) {
+    LOG.info("XML to avro conversion completed for {}, file was created - {}", datasetUuid, isFileCreated);
 
-      LOG.info("Parsing process has been started");
-
-      ExtendedRecordConverter.crete(configuration.xmlReaderParallelism)
-        .toAvroFromXmlResponse(paths.getInputPath().toString(), dataFileWriter);
-
-      LOG.info("Parsing process has been finished");
-
-    } catch (IOException ex) {
-      LOG.error("Failed performing conversion on {}", datasetUuid, ex);
-      throw new IllegalStateException("Failed performing conversion on " + datasetUuid, ex);
-    } finally {
-      isFileDeleted = FileSystemUtils.deleteAvroFileIfEmpty(fs, paths.getOutputPath());
-    }
-
-    LOG.info("XML to avro conversion completed for {}, file was deleted - {}", datasetUuid, isFileDeleted);
-    if (!isFileDeleted && Objects.nonNull(publisher)) {
+    if (isFileCreated && Objects.nonNull(publisher)) {
       try {
         URI uri = paths.getOutputPath().toUri();
         publisher.send(new ExtendedRecordAvailableMessage(datasetUuid, message.getAttempt(), uri, configuration.interpretTypes));
@@ -87,5 +60,4 @@ public class XmlToAvroCallBack extends AbstractMessageCallback<CrawlFinishedMess
       }
     }
   }
-
 }
