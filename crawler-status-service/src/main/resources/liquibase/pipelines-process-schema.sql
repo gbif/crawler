34d995de6711f71a8dc4a22429ba009d0b9b8385
fmendezh <federicomh@gmail.com>
2019-08-28 10:08:23 +0200

    changing table name from plural to singular

--- crawler-status-service/src/main/resources/liquibase/pipelines-process-schema.sql
+++ crawler-status-service/src/main/resources/liquibase/pipelines-process-schema.sql
@@ -1,6 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS hstore;
 
-CREATE TABLE pipelines_process (
+CREATE TABLE pipeline_process (
  key bigserial NOT NULL PRIMARY KEY,
  dataset_key uuid NOT NULL,
  attempt integer NOT NULL,
@@ -10,21 +10,21 @@ CREATE TABLE pipelines_process (
  UNIQUE(dataset_key, attempt)
 );
 
-CREATE TYPE pipelines_step_status AS ENUM ('SUBMITTED', 'RUNNING', 'FAILED', 'COMPLETED');
+CREATE TYPE pipeline_step_status AS ENUM ('SUBMITTED', 'RUNNING', 'FAILED', 'COMPLETED');
 
-CREATE TYPE pipelines_step_name AS ENUM ('DWCA_TO_VERBATIM', 'XML_TO_VERBATIM', 'ABCD_TO_VERBATIM', 'VERBATIM_TO_INTERPRETED', 'INTERPRETED_TO_INDEX', 'HIVE_VIEW');
+CREATE TYPE pipeline_step_name AS ENUM ('DWCA_TO_VERBATIM', 'XML_TO_VERBATIM', 'ABCD_TO_VERBATIM', 'VERBATIM_TO_INTERPRETED', 'INTERPRETED_TO_INDEX', 'HIVE_VIEW');
 
-CREATE TABLE pipelines_step(
+CREATE TABLE pipeline_step(
  key bigserial NOT NULL PRIMARY KEY,
- name pipelines_step_name NOT NULL,
+ name pipeline_step_name NOT NULL,
  runner text,
  started timestamp with time zone,
  finished timestamp with time zone,
- state pipelines_step_status NOT NULL,
+ state pipeline_step_status NOT NULL,
  message text,
  metrics hstore,
  rerun_reason text,
  created timestamp with time zone NOT NULL DEFAULT now(),
  created_by text NOT NULL,
- pipelines_process_key integer NOT NULL REFERENCES pipelines_process (key) ON DELETE CASCADE
+ pipeline_process_key integer NOT NULL REFERENCES pipeline_process (key) ON DELETE CASCADE
 );
