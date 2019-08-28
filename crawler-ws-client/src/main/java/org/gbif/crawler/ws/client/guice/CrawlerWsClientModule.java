8b55ec205c591a2cb5cb26e7cb0f9d764c0c4be2
fmendezh <federicomh@gmail.com>
2019-08-28 15:53:21 +0200

    optionally adding the GbifAuthAppModule

--- crawler-ws-client/src/main/java/org/gbif/crawler/ws/client/guice/CrawlerWsClientModule.java
+++ crawler-ws-client/src/main/java/org/gbif/crawler/ws/client/guice/CrawlerWsClientModule.java
@@ -2,6 +2,7 @@ package org.gbif.crawler.ws.client.guice;
 
 import org.gbif.api.service.crawler.DatasetProcessService;
 import org.gbif.crawler.ws.client.CrawlerWsClient;
+import org.gbif.ws.client.guice.GbifApplicationAuthModule;
 import org.gbif.ws.client.guice.GbifWsClientModule;
 
 import java.util.Properties;
@@ -11,6 +12,9 @@ import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
+
+import static org.gbif.ws.client.guice.GbifApplicationAuthModule.PROPERTY_APP_KEY;
+import static org.gbif.ws.client.guice.GbifApplicationAuthModule.PROPERTY_APP_SECRET;
 
 /**
  * Guice module that exposes the Crawler Service.
@@ -23,6 +27,9 @@ public class CrawlerWsClientModule extends GbifWsClientModule {
 
   @Override
   protected void configureClient() {
+    if (getProperties().containsKey(PROPERTY_APP_KEY) && getProperties().containsKey(PROPERTY_APP_SECRET)) {
+      install(new GbifApplicationAuthModule(getProperties()));
+    }
     bind(DatasetProcessService.class).to(CrawlerWsClient.class);
     expose(DatasetProcessService.class);
   }
@@ -35,4 +42,5 @@ public class CrawlerWsClientModule extends GbifWsClientModule {
   public WebResource provideWebResource(Client client, @Named("crawler.ws.url") String url) {
     return client.resource(url);
   }
+
 }
