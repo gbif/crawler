9d0fac3c982ec08db358543a90f7a2b35515fbad
fmendezh <federicomh@gmail.com>
2020-06-08 12:58:09 +0200

    adding missing configure overwrite

--- crawler-ws/src/main/java/org/gbif/crawler/ws/config/WebSecurityConfig.java
+++ crawler-ws/src/main/java/org/gbif/crawler/ws/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -14,6 +15,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.httpBasic().disable()
+      .cors();
+  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
