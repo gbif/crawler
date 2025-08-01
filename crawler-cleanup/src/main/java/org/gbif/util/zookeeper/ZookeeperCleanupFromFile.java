18c8fd65e988fb2b46e1273d68c16415c7443d0f
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2025-08-01 10:32:56 +0200

    updated envs in ZookeeperCleanupFromFile

--- crawler-cleanup/src/main/java/org/gbif/util/zookeeper/ZookeeperCleanupFromFile.java
+++ crawler-cleanup/src/main/java/org/gbif/util/zookeeper/ZookeeperCleanupFromFile.java
@@ -25,29 +25,17 @@ public class ZookeeperCleanupFromFile {
 
   private static final Logger LOG = LoggerFactory.getLogger(ZookeeperCleanupFromFile.class);
   private static final String PROD = "prod";
-  private static final String UAT = "uat";
   private static final String DEV = "dev";
-  private static final String DEV2 = "dev2";
-  private static final String UAT2 = "uat2";
   private static final String TEST = "test";
   private static final String PROD_PATH = "/prod_crawler/crawls/";
-  private static final String UAT_PATH = "/uat_crawler/crawls/";
-  private static final String UAT2_PATH = "/uat2_crawler/crawls/";
-  private static final String DEV_PATH = "/dev_crawler/crawls/";
-  private static final String DEV2_PATH = "/dev2_crawler/crawls/";
-  private static final String TEST_PATH = "/uat_crawler/crawls/";
+  private static final String DEV_PATH = "/dev_crawler/crawls/";;
+  private static final String TEST_PATH = "/test_crawler/crawls/";
   private static final String PROD_ZK =
       "c5zk1.gbif.org:2181,c5zk2.gbif.org:2181,c5zk3.gbif.org:2181";
-  private static final String UAT_ZK =
-      "gbif-zookeeper-server-default-0.gbif-zookeeper-server-default.uat.svc.cluster.local:2282,gbif-zookeeper-server-default-1.gbif-zookeeper-server-default.uat.svc.cluster.local:2282,gbif-zookeeper-server-default-2.gbif-zookeeper-server-default.uat.svc.cluster.local:2282,gbif-zookeeper-server-default-3.gbif-zookeeper-server-default.uat.svc.cluster.local:2282,gbif-zookeeper-server-default-4.gbif-zookeeper-server-default.uat.svc.cluster.local:2282";
   private static final String DEV_ZK =
-      "c3zk1.gbif-dev.org:2181,c3zk2.gbif-dev.org:2181,c3zk3.gbif-dev.org:2181";
-  private static final String DEV2_ZK =
-      "gbif-zookeeper-server-default-0.gbif-zookeeper-server-default.gbif-develop.svc.cluster.local:2282,gbif-zookeeper-server-default-1.gbif-zookeeper-server-default.gbif-develop.svc.cluster.local:2282,gbif-zookeeper-server-default-2.gbif-zookeeper-server-default.gbif-develop.svc.cluster.local:2282";
-  private static final String UAT2_ZK =
-      "c8n1.gbif.org:31930,c8n2.gbif.org:31930,c8n3.gbif.org:31930,c8n5.gbif.org:31930,c8n9.gbif.org:31930";
+      "c5n7.gbif-test.org:31706,c5n9.gbif-test.org:31706,c6n8.gbif-test.org:31706";
   private static final String TEST_ZK =
-      "c6n8.gbif-test.org:31706,c6n7.gbif-test.org:31706,c5n8.gbif-test.org:31706";
+      "c5n7.gbif-test.org:31706,c5n9.gbif-test.org:31706,c6n8.gbif-test.org:31706";
 
   private ZookeeperCleanupFromFile() {}
 
@@ -55,7 +43,7 @@ public class ZookeeperCleanupFromFile {
   public static void main(String[] args) throws IOException, InterruptedException {
     LOG.debug("ZookeeperCleanupFromFile starting");
     if (args.length != 2) {
-      LOG.error("Usage: ZookeeperCleanupFromFile <filename> <environment: prod, uat, dev or dev2>");
+      LOG.error("Usage: ZookeeperCleanupFromFile <filename> <environment: prod, test or dev>");
       System.exit(1);
     }
 
@@ -66,28 +54,16 @@ public class ZookeeperCleanupFromFile {
         path = PROD_PATH;
         zkPath = PROD_ZK;
         break;
-      case UAT:
-        path = UAT_PATH;
-        zkPath = UAT_ZK;
-        break;
       case DEV:
         path = DEV_PATH;
         zkPath = DEV_ZK;
-        break;
-      case DEV2:
-        path = DEV2_PATH;
-        zkPath = DEV2_ZK;
-        break;
-      case UAT2:
-        path = UAT2_PATH;
-        zkPath = UAT2_ZK;
         break;
       case TEST:
         path = TEST_PATH;
         zkPath = TEST_ZK;
         break;
       default:
-        throw new IllegalArgumentException("Environment must be one of: prod, uat, dev, dev2 or uat2");
+        throw new IllegalArgumentException("Environment must be one of: prod, test or dev");
     }
 
     List<String> keys = HueCsvReader.readKeys(args[0]);
